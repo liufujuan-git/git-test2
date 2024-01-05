ea7db3a19919c584d95de0686f61b2f4f7970384
20220903329 <20220903329@git.com>
2024-01-05 17:15:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/UserTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author liufujuan
+ * @create 2024-01-05 17:14
+ */
+public class UserTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
